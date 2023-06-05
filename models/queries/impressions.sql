f2f07d03baba9a2cdab702674c11afe14f41d328
Amir Shabani <amishbni@gmail.com>
2023-06-05 13:36:17 +0000

    Calculate impressions

---
+++ models/queries/impressions.sql
@@ -0,0 +1,18 @@
+with
+    impressions_view as (
+        select channel, impressions
+        from {{ ref("source_twitter") }}
+        union all
+        select channel, impressions
+        from {{ ref("source_facebook") }}
+        union all
+        select channel, impressions
+        from {{ ref("source_bing") }}
+        union all
+        select channel, impressions
+        from {{ ref("source_tiktok") }}
+    )
+
+select channel, sum(impressions) as impressions
+from impressions_view
+group by channel
